e4c6ff5acb549d4eb4a4242ce00cb023b0d7854a
ShanthalaRa0 <shanthala.enon@gmail.com>
2026-07-22 13:04:03 +0900

    Server side router name change

--- rag-server/main.py
+++ rag-server/main.py
@@ -4,7 +4,7 @@ from api.routes.upload_routes import router as upload_router
 from api.routes.query_routes import router as query_router
 from api.routes.status_routes import router as status_router
 #from api.routes.websocket_routes import router as websocket_router
-from websocket.websocket_routes import router as websocket_router
+from websocket.router import router as websocket_router
 
 from api.middleware.logging_middleware import LoggingMiddleware
 
